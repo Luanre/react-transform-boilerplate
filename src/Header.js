ff2ad71ecc655d02f809a4012eb19c034f4b8a0d
Nikolay Nadorichev <n.nadorichev@cti.ru>
2015-09-14 19:35:17 +0300

    wip with css-modules

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import styles from './HeaderStyles.css';
 
 export default class Header extends Component {
     render () {
         return (
             <div>
-                <Link to="home">Home</Link> | <Link to="gallery">Gallery</Link> | <Link to="todolist">ToDo list</Link>
+                <li className={styles.item}><Link to="home">Home</Link></li>
+                <li className={styles.item}><Link to="gallery">Gallery</Link></li>
+                <li className={styles.item}><Link to="todolist">ToDo list</Link></li>
                 <hr />
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'eval',
@@ -13,14 +14,24 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
+    new ExtractTextPlugin('style.css', { allChunks: true }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+
+  postcss: [
+    require('autoprefixer'),
+    require('postcss-color-rebeccapurple')
+  ],
+
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
     }]
   }
 };
